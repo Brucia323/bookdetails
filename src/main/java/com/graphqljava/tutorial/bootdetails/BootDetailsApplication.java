7a0e2399a79d8c1242fb9b3c38be19912c3830cf
周晨阳 <1448517833@qq.com>
2022-11-22 14:28:07 +0800

    Initial commit

---
+++ src/main/java/com/graphqljava/tutorial/bootdetails/BootDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bootdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDetailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graphqljava/tutorial/bootdetails/BootDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bootdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
